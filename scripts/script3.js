88da2b2348f164293d2c45d39d81007206ec09f0
Emerenz <chienperdu2@gmail.com>
2021-04-02 22:53:23 +0200

    Add files via upload

--- scripts/script3.js
+++ scripts/script3.js
@@ -1,22 +1,47 @@
 var images = ['pal1.png', 'pal3.png', 'pal2.png', 'pal4.png']
+var dancimages = ['paldance1.png', 'paldance2.png', 'paldance3.png', 'paldance4.png', 'paldance5.png', 'paldance6.png', 'paldance7.png', 'paldance8.png']
 var backimages = ['pal4.png', 'pal2.png', 'pal3.png', 'pal1.png']
+var speakarray = ['palall.png','palspeak2.png'];
 
 var current = 0;
 var position = 10;
 var step=12;
 var doesitgo = false;
 var isspeaking = false;
+var isdancing = false;
 var goingback = false;
 var firstspeak = true;
 var currentspeak = 0;
+var currentdance = 0;
 var wasgoingback = false;
-var speakarray = ['palall.png','palspeak2.png'];
+var audioElement = new Audio('levan Polka.mp3');
+
+
+
+function dancing()
+{
+
+
+ if (isdancing) return;
+ audioElement.play();
+  isdancing=true;
+  image.src = "Gallery/" + images[current];
+  // image.style.left = 10 + "px";
+clearInterval(rep);
+rep = setInterval(paldance, 120);
+
+
+}
+
+
+
 
 function comeback()
 {
 
  if (goingback) return;
   goingback=true;
+    isdancing=false;
   image.src = "Gallery/" + images[current];
   // image.style.left = 10 + "px";
 image.src = "Gallery/palall.png";
@@ -58,12 +83,27 @@ image.src = "Gallery/" + speakarray[currentspeak];
 
 }
 
+function paldance() {
+
+
+  console.log(currentdance);
+
+  var image = document.getElementById('image');
+    console.log(position + "px");
+image.style.left = position + "px";
+image.src = "Gallery/" + dancimages[currentdance];
+currentdance = (currentdance+1)%8;
+
+}
+
 
 var rep;
 
 function stop() {
   isspeaking = false;
   doesitgo = false;
+  audioElement.pause();
+    isdancing=false;
   if (goingback) wasgoingback = true;
   goingback=false;
   var image = document.getElementById('image');
@@ -79,6 +119,7 @@ function start() {
   if (doesitgo) return;
   goingback=false;
   doesitgo = true;
+    isdancing=false;
   isspeaking = false;
   wasgoingback = false;
   var current = 0;
@@ -100,6 +141,7 @@ function speak() {
   firstspeak=false;
   doesitgo = false;
   isspeaking = (true);
+    isdancing=false;
   clearInterval(rep);
   console.log(position);
 
